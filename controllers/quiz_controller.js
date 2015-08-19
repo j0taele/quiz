88d7ede1241aac0991a80b2870d727e7aaf0fc6a
j0taele <jot4ele@gmail.com>
2015-08-19 08:59:07 +0200

    Busquedas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,11 +14,19 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(
-    function(quizes) {
-      res.render('quizes/index.ejs', {quizes: quizes, errors: []});
+  var search;
+
+  if(req.query.search){
+    search = "%" + req.query.search.replace(/\s/g, "%") + "%";
+  } else {
+    search = "%%";
+  };
+    
+  models.Quiz.findAll({where: ["pregunta like ?", search]}).then(
+    function(quizes){
+      res.render('quizes/index.ejs', { quizes: quizes, errors: []});
     }
-  ).catch(function(error){next(error)});
+  ).catch(function(error) { next(error)});
 };
 
 // GET /quizes/:id
